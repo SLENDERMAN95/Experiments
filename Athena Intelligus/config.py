b02b76f03896b780a5ade894c93322bb08cf6d60
SLENDERMAN95 <116130277+SLENDERMAN95@users.noreply.github.com>
2022-12-17 14:14:13 -0700

    Template config file
    Just put your own API keys and sounds here

---
+++ Athena Intelligus/config.py
@@ -0,0 +1,14 @@
+#Spotify
+client_id = "client_id api key here"
+client_secret = "client_secret api key here"
+#Weather
+api_key = "weather api key"
+#Kasa
+bulb1 = "bulb ip here"
+
+#Sounds
+from playsound import playsound
+
+
+def function_sound():
+    playsound('./Ding.mp3')
