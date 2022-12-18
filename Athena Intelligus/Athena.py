b70d66e7e4d8c9640938c949bea9cff3d868a839
SLENDERMAN95 <116130277+SLENDERMAN95@users.noreply.github.com>
2022-12-17 18:29:11 -0700

    Wookiepedia issue
    Requests to google are returning error 429 (Too many requests). Attempted to circumvent by randomizing User Agent

--- Athena Intelligus/Athena.py
+++ Athena Intelligus/Athena.py
@@ -1,7 +1,6 @@
 import subprocess
-
 from bs4 import BeautifulSoup
-import requests
+import requests, random
 from googlesearch import search
 import pyttsx3
 import speech_recognition as speech
@@ -202,8 +201,23 @@ def what_time():
     elif hour >= 16 and hour < 23:
         speak(f"Good Evening! the time is {time}")
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36'}
+def rand_agent():
+    user_agent_list = [
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:77.0) Gecko/20100101 Firefox/77.0',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
+    ]
+    for _ in user_agent_list:
+        # Pick a random user agent
+        user_agent = random.choice(user_agent_list)
+        headers = {'User-Agent': user_agent}
+
+    return headers
+
+
+#headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.54'}
 URL = ''
 
 #Location Function
@@ -334,6 +348,7 @@ def listen():
 
 if __name__ == "__main__":
     greet()
+    headers = rand_agent()
     city, country, latitude, longitude = get_location()
     #kasa_discover()
 
