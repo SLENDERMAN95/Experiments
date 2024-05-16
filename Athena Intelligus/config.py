fe65b830d29ccb041550bd659841efb969ab0394
SLENDERMAN95 <116130277+SLENDERMAN95@users.noreply.github.com>
2024-05-15 20:48:16 -0600

    Update config.py
    Added where to get API keys and removed playsound function, very broken

--- Athena Intelligus/config.py
+++ Athena Intelligus/config.py
@@ -1,14 +1,7 @@
-#Spotify
+#Spotify (https://developer.spotify.com/dashboard, create an app and paste the Client ID and Secrets here. Do not share these)
 client_id = "client_id api key here"
 client_secret = "client_secret api key here"
-#Weather
+#Weather (https://openweathermap.org/ and generate your API Key)
 api_key = "weather api key"
-#Kasa
+#Kasa (Will need to use the Kasa Discover function to get IP of lights. Working on making this automatic)
 bulb1 = "bulb ip here"
-
-#Sounds
-from playsound import playsound
-
-
-def function_sound():
-    playsound('./Ding.mp3')
